a4f25fff4fada5207d8f11c1c047353b7d163df1
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-06 14:41:52 +0300

    numeric filters implementation was adde

--- controllers/products.js
+++ controllers/products.js
@@ -6,8 +6,9 @@ const getAllProductsStatic = async (req, res) => {
   });
   res.status(200).json({ nbHits: products.length, products });
 };
+
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -20,6 +21,31 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
+  // numeric filters
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    const reqEx = /\b(<|>|=|>=|<=)\b/g;
+    let filters = numericFilters.replace(
+      reqEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
   let result = Product.find(queryObject); //await was removed
 
   // sorting feature
@@ -29,11 +55,13 @@ const getAllProducts = async (req, res) => {
     result = result.sort("createdAt");
   }
 
+  // fields
   if (fields) {
     const fieldList = fields.split(",").join(" ");
     result = result.select(fieldList);
   }
 
+  // pagination and limiter
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
