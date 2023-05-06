cb49dc0299fbc4822056f665b011a84153acbcd8
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-06 13:01:14 +0300

    basic filtering by featured, company and name fields was implemented

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,22 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ nbHits: products.length, products });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ message: "products route" });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  const products = await Product.find(queryObject);
+
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
