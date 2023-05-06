3cf6466b99b5a7099b67e3f315e1ac823c0237b2
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-06 14:19:44 +0300

    pagination and per page item limitation were added

--- controllers/products.js
+++ controllers/products.js
@@ -34,6 +34,12 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldList);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   // getting the result
   const products = await result;
 
