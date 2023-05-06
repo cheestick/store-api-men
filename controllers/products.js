227d2df95d3ffc4a839e1d5d5ec48857bbba3a3d
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-06 13:51:18 +0300

    responce fields selector was added

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ nbHits: products.length, products });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -20,14 +20,21 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  let result = Product.find(queryObject);
+  let result = Product.find(queryObject); //await was removed
 
+  // sorting feature
   if (sort) {
     result = result.sort(sort.split(",").join(" "));
   } else {
     result = result.sort("createdAt");
   }
 
+  if (fields) {
+    const fieldList = fields.split(",").join(" ");
+    result = result.select(fieldList);
+  }
+
+  // getting the result
   const products = await result;
 
   res.status(200).json({ nbHits: products.length, products });
